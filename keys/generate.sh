ed2bfdebfbbe3f58289ab1bf51b9ecc4c275a468
Tobias Derksen <t.derksen@mailbox.org>
2020-01-26 13:41:44 +0100

    Added keys

---
+++ keys/generate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+TF="keys.tf"
+echo "" > "$TF"
+NUM=${1:-10}
+
+append() {
+    echo "$1" >> "$TF"
+}
+
+provider() {
+    append 'provider "aws" {'
+    append '  region = "eu-central-1"'
+    append '  version = "~> 2.7"'
+    append '}'
+    append ''
+}
+
+add_key() {
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "Invalid parameter" > /dev/stderr
+        exit 5
+    fi
+    PUB_KEY="${2}"
+
+    append "resource \"aws_key_pair\" \"public-key-$1\" {"
+    append "  key_name = \"training-$1\""
+    append "  public_key = \"${PUB_KEY}\""
+    append '}'
+    append ''
+}
+
+provider
+
+HOST_KEY=$(cat "$HOME/.ssh/id_rsa.pub")
+add_key "0" "$HOST_KEY"
+
+for i in $(seq 1 $NUM); do
+    FILENAME="training$i"
+    if [[ ! -f "${FILENAME}" ]]; then
+        echo "Generate key $i to file: '${FILENAME}'"
+        ssh-keygen -t rsa -b 4096 -C "Training ${i}" -P '' -f "${FILENAME}" > /dev/null
+    else
+        echo "Key ${FILENAME} already exists"
+    fi
+    
+    PUB_KEY=$(cat "training${i}.pub")
+    add_key "$i" "${PUB_KEY}" 
+done
+
+terraform init
+
+if [[ $? -eq 0 ]]; then
+    terraform apply
+fi
+
+exit $?
